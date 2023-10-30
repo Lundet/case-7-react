1a6c22d4eecdfb67e0770b8316ecadff2543175b
Lundet <hannes.j.lundin@gmail.com>
2023-10-30 13:25:36 +0100

    Fetching in app.js instead of home,and sending it as props instead

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Booking from './pages/Booking'; 
-
+import Booking from './pages/Booking';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
+  const [cinemaData, setCinemaData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('cinema.json');
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        setCinemaData(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Router>
-      <div> 
-        <Routes>         
-          <Route path="/" element={<Home />} />
-          <Route path="/booking" element={<Booking/>} />        
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home cinemaData={cinemaData} loading={loading} error={error} />}
+          />
+          <Route path="/booking" element={<Booking />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,103 +1,42 @@
-import { useState, useEffect } from 'react'
 import React from 'react';
-import '../styles/Home.css'
-import ListSeatings from '../components/ListSeatings';
+import '../styles/Home.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-
-function Home() {
-  // state to store cinema data
-  const [cinemaData, setCinemaData] = useState(null);
-
-  // state to track if application is loading
-  const [loading, setLoading] = useState(true);
-
-  // state to track any error message
-  const [error, setError] = useState(null);
-
-
-
-  // function listSeatings() {
-
-  //   return (
-  //     <div> 
-  //       <h3>Seats:</h3>
-  //       <ul>
-  //         {cinemaData.cinema.movies.shows.seats.map((seat, seatIndex) => (
-  //           <li key={seatIndex}>Seat {seat.seatNumber}</li>
-  //         ))}
-  //       </ul>
-  //     </div>
-  //   );
-  // }
-
-  // trigger the arrow function inside useEffect ONE time before loading
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('cinema.json');
-
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        setCinemaData(data);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  // check if loading, if true then we should display a loading message
+function Home({ cinemaData, loading, error }) {
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  // check if there is an error, if true then we should display it
   if (error) {
     return <div>Error: {error.message}</div>;
   }
 
   return (
     <div>
-    <Header />
-    <main>
-      <h1>{cinemaData.cinema.name}</h1>
-      <h2>Movies Right Now</h2>
-      <ul>
-        {cinemaData.cinema.movies.map((movie, movieIndex) => (
-          <li key={movieIndex}>
-            <img src={movie.image} alt="" width="150px" />
-            <p>Title: {movie.title}</p>
-            <p>Duration: {movie.duration}</p>
+      <Header />
+      <main>
+        <h1>{cinemaData.cinema.name}</h1>
+        <h2>Movies Right Now</h2>
+        <ul>
+          {cinemaData.cinema.movies.map((movie, movieIndex) => (
+            <li key={movieIndex}>
+              <img src={movie.image} alt="" width="150px" />
+              <p>Title: {movie.title}</p>
+              <p>Duration: {movie.duration}</p>
 
-            {movie.shows.map((show, showIndex) => (
-              <div key={showIndex}>
-                <p>Room: {show.room}</p>
-                <p>Time: {show.time}</p>
-                {/* <p>
-                    {show.seats.map((seat, seatIndex) => (
-                      <p key={seatIndex}>{seat.seatNumber}</p>
-                    ))}
-                  </p> */}
-                <button className='book-now'>Book</button>
-
-
-              </div>
-            ))}
-
-          </li>
-        ))}
-      </ul>
-      
-    </main>
-    <Footer />
+              {movie.shows.map((show, showIndex) => (
+                <div key={showIndex}>
+                  <p>Room: {show.room}</p>
+                  <p>Time: {show.time}</p>
+                  <a className='read-more' href="booking"> <button className='read-more'>Read more</button></a>
+                </div>
+              ))}
+            </li>
+          ))}
+        </ul>
+      </main>
+      <Footer />
     </div>
   );
 }
